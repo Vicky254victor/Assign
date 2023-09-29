21f92549ce45949d1224cf942c4bb6a887e19e07
Vicky254victor <146352972+Vicky254victor@users.noreply.github.com>
2023-09-29 13:17:54 +0300

    Add files via upload

---
+++ assignment exercise.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b, d, c;
+    printf("Enter the first value:" );
+    scanf("%d" , &a);
+    printf("enter the second value:");
+    scanf("%d",&b);
+    printf("Enter the third value");
+    scanf("%d" , &d);
+    c=a+b+d;
+    printf("%d+%d+%d=%d\n",a,b,d,c );
+    return 0;}
+
+
+
+
+
+
+
+
